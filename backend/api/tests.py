9174b7599dc3b5a68f1dc2f4948b597885aa78a5
Aleksandr Efremov <zemort@yandex.ru>
2025-10-08 19:52:00 +0300

    tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,8 +1,11 @@
 # backend/api/tests.py
 from http import HTTPStatus
 
-from api import models
 from django.test import Client, TestCase
+
+from api import models
+
+
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
