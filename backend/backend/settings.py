5606eef1fe418e373413b668dc4501e58c8c9d78
Aleksandr Efremov <zemort@yandex.ru>
2025-10-13 11:45:05 +0300

    Исправления

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/backend_static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
