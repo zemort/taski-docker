9ee1307017439dde5f55e5451b5526cbb535dfcb
Aleksandr Efremov <zemort@yandex.ru>
2025-10-13 12:30:29 +0300

    Исправления

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,9 @@ SECRET_KEY = (
     'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 )
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['89.169.166.136', 'localhost', '127.0.0.1', 'myhosttestrus.ddns.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.166.136', 'myhosttestrus.ddns.net']
 
 
 # Application definition
